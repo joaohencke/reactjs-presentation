07c45ec047976ec0f2985cb4e1d630653f672c86
Joao Hencke <joao.hencke@gmail.com>
2019-06-20 10:52:32 -0300

    :hammer: useEffect hooks

--- src/containers/Counter.jsx
+++ src/containers/Counter.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, GenericButton } from "../components";
 
 function CounterContainer() {
   const [counter, setCounter] = useState(0);
 
   const increment = () => setCounter(counter + 1);
+
+  useEffect(() => {
+      console.log('counter renderizado')
+      return () => {
+          console.log('counter desmontado');
+      }
+  }, [counter]);
+
+  useEffect(() => {
+    console.log('componente renderizado');
+    return () => {
+        console.log('componente desmontado');
+    }
+  }, [])
 
   return (
     <section>
