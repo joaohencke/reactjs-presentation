f7390ec58ef329ac424331adda8e5611982ec347
Joao Hencke <joao.hencke@gmail.com>
2019-06-20 10:02:57 -0300

    :hammer: counter using class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      counter: 0
+    };
+
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <section>
+        <section className="header">
+          <h3>Contador</h3>
+        </section>
+        <section className="content">Clicado {counter}x</section>
+        <section className="action">
+          <button type="button" onClick={this.increment}>
+            Incrementar
+          </button>
+        </section>
+      </section>
+    );
+  }
 }
 
 export default App;
