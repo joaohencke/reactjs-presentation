8edc63c0912392009b808aff5fe9a6adac0e729e
Joao Hencke <joao.hencke@gmail.com>
2019-06-20 09:56:05 -0300

    :hammer: counter using hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <section>
+      <section className="header">
+        <h3>Contador</h3>
+      </section>
+      <section className="content">Clicado {counter}x</section>
+      <section className="action">
+        <button type="button" onClick={increment}>
+          Incrementar
+        </button>
+      </section>
+    </section>
   );
 }
 
